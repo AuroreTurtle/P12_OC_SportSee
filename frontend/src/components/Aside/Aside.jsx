c8c2d34eaba66bc4d26802c39ba9a0a4fdc2571b
Aurore GOURRAUD <aurore.gourraud@gmail.com>
2022-10-12 18:10:46 +0200

    Add Aside component

---
+++ frontend/src/components/Aside/Aside.jsx
@@ -0,0 +1,32 @@
+import "./Aside.css";
+import Yoga from "../../assets/sports/yoga.svg";
+import Swim from "../../assets/sports/swim.svg";
+import Bike from "../../assets/sports/bike.svg";
+import Fitness from "../../assets/sports/fitness.svg";
+
+function Aside() {
+    return (
+        <aside>
+            <div id="aside">
+                <div></div>
+                <ul>
+                    <li className="aside-icon">
+                        <img src={Yoga} alt="Icône Yoga" />
+                    </li>
+                    <li className="aside-icon">
+                        <img src={Swim} alt="Icône Natation" />
+                    </li>
+                    <li className="aside-icon">
+                        <img src={Bike} alt="Icône Vélo" />
+                    </li>
+                    <li className="aside-icon">
+                        <img src={Fitness} alt="Icône Fitness" />
+                    </li>
+                </ul>
+                <p id="aside-copyright">Copyright, SportSee 2020</p>
+            </div>
+        </aside>
+    );
+}
+
+export default Aside;
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style/style.css";
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
+import Aside from "./components/Aside/Aside";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Header />
+        <Aside />
     </React.StrictMode>
 );
